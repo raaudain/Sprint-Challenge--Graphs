087770974c529fc5a52d36cef837efe30e7d0b98
Ramon Audain <raudain123@gmail.com>
2020-04-16 16:12:58 -0700

    i'm getting somewhere

--- adv.py
+++ adv.py
@@ -2,17 +2,19 @@ from room import Room
 from player import Player
 from world import World
 
+import sys
+sys.path.append("../Graphs/projects/graph")
+from util import Queue, Stack
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
-
-room = Room()
+#room = Room()
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+#map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -24,12 +26,64 @@ world.load_graph(room_graph)
 # Print an ASCII map
 world.print_rooms()
 
+
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+def dft(starting_room):
+    #room = Room()
+    
+    stack = Stack()
+    stack.push([starting_room.id])
+    
+    
+    
+    visited = set()
+    
+    while stack.size() > 0:
+        path = stack.pop()
+        vertex = path[-1]
+        print(vertex)
+        
+        # if vertex not in visited:
+        #     print(vertex, player)
+        #     visited.add(vertex)
+            
+        for step in world.rooms:
+            if step not in visited:
+                print(vertex, player)
+                visited.add(step)
+            
+            print(step, player)
+            temp_path = path.copy()
+            temp_path.append(step)
+            stack.push(temp_path)
+    return
 
+# def bft(starting_room):
+#     visited = set()
+#     queue = Queue()
+#     queue.enqueue([starting_room.id])
+    
+#     while queue.size() > 0:
+#         path = queue.dequeue()
+#         vertex = path[-1]
+#         # test = room.id
+        
+#         if vertex not in visited:
+#             print(vertex)
+#             visited.add(vertex)
+            
+#             for step in world.rooms:
+#                 print(step, world.rooms[vertex])
+#                 temp_path = path.copy()
+#                 temp_path.append(step)
+#                 queue.enqueue(temp_path)
+
+# Fill this out with directions to walk
+#traversal_path = ['n', 'n']
+traversal_path = dft(player.current_room)
+
+#print(traversal_path)
 
 
 # TRAVERSAL TEST
